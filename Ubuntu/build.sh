7b4fab5eaacf0b05870d9b82221e0deed1421140
root <root@cloud-server-22.(none)>
2014-10-09 10:56:26 -0400

    build all Linux flavors from R-3.1.1.tar.gz [ci skip]

--- Ubuntu/build.sh
+++ Ubuntu/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 mkdir rpmbuild/SOURCES
-cp ../RRO-3.1.1.tar.gz rpmbuild/SOURCES
+cp ../R-3.1.1.tar.gz rpmbuild/SOURCES
 cp ../packages/RevoBase.tar.gz rpmbuild
 HOME=`pwd`
 export HOME
-cd rpmbuild
+cd rpmbuild/SPECS
+tar xzf R-3.1.1.tar.gz
+mv R-3.1.1 RRO-3.1.1
+tar czf RRO-3.1.1.tar.gz RRO-3.1.1
+rm R-3.1.1.tar.gz
+cd ../
 rpmbuild -ba SPECS/R.spec
 cd RPMS/x86_64
 alien --scripts --to-deb RRO-3.1.1-1.x86_64.rpm
--- openSUSE/build.sh
+++ openSUSE/build.sh
@@ -5,5 +5,10 @@ echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,BUILDROOT,SRPMS}
 cp ../RRO-3.1.1.tar.gz rpmbuild/SOURCES
 cp ../packages/RevoBase.tar.gz rpmbuild
-cd rpmbuild
+cd rpmbuild/SPECS
+tar xzf R-3.1.1.tar.gz
+mv R-3.1.1 RRO-3.1.1
+tar czf RRO-3.1.1.tar.gz RRO-3.1.1
+rm R-3.1.1.tar.gz
+cd ../
 rpmbuild -ba SPECS/R.spec

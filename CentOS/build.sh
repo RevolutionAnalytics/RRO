5aa7c72297f10a6bf181f0060658cd0e4ff4434c
root <root@cloud-server-22.(none)>
2014-10-09 11:23:19 -0400

    build OSX from R-3.1.1.tar.gz [ci skip]

--- CentOS/build.sh
+++ CentOS/build.sh
@@ -5,7 +5,7 @@ echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,BUILDROOT,SRPMS}
 cp ../R-3.1.1.tar.gz rpmbuild/SOURCES
 cp ../packages/RevoBase.tar.gz rpmbuild
-cd rpmbuild/SPECS
+cd rpmbuild/SOURCES
 tar xzf R-3.1.1.tar.gz
 mv R-3.1.1 RRO-3.1.1
 tar czf RRO-3.1.1.tar.gz RRO-3.1.1

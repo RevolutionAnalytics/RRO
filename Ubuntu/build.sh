2401c92f7ff101db1a1bb91992fd41ddd51de4eb
root <root@cloud-server-22.(none)>
2014-10-09 11:24:57 -0400

    build Ubuntu and openSUSE from R-3.1.1.tar.gz [ci skip]

--- Ubuntu/build.sh
+++ Ubuntu/build.sh
@@ -4,7 +4,7 @@ cp ../R-3.1.1.tar.gz rpmbuild/SOURCES
 cp ../packages/RevoBase.tar.gz rpmbuild
 HOME=`pwd`
 export HOME
-cd rpmbuild/SPECS
+cd rpmbuild/SOURCES
 tar xzf R-3.1.1.tar.gz
 mv R-3.1.1 RRO-3.1.1
 tar czf RRO-3.1.1.tar.gz RRO-3.1.1

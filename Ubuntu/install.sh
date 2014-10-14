d10fbb99744aa66e7739a5939a8fef7c916e9108
root <root@cloud-server-22.(none)>
2014-10-14 19:33:53 -0400

    add required packages for Ubuntu [ci skip]

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -3,4 +3,5 @@ echo "Installing RRO-8.0-Beta"
 dpkg -i RRO-8.0-Beta-Ubuntu-14.04.x86_64.deb 2>/dev/null 1>/dev/null
 if [ "$?" -ne 0 ]; then 
 apt-get -y -f install
+apt-get -y gcc make gfortran-4.8 liblzma-dev
 fi

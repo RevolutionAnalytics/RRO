3befef0d80297a20dfc38c7d0230f0a6208cd3c6
root <root@cloud-server-22.(none)>
2014-10-14 21:08:56 -0400

    fix apt-get command Ubuntu [ci skip]

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -4,4 +4,7 @@ dpkg -i RRO-8.0-Beta-Ubuntu-14.04.x86_64.deb 2>/dev/null 1>/dev/null
 if [ "$?" -ne 0 ]; then 
 apt-get -y -f install
 fi
-apt-get -y install gcc make gfortran-4.8 liblzma-dev
+apt-get -y install gcc 
+apt-get -y install make 
+apt-get -y install gfortran-4.8 
+apt-get -y install liblzma-dev

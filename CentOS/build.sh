53991e3f9856b5aa32635205dee5dc718391a791
root <root@cloud-server-22.(none)>
2014-11-18 18:49:44 -0500

    update build version to 8.0.1 [ci skip]

--- CentOS/build.sh
+++ CentOS/build.sh
@@ -4,11 +4,11 @@ export HOME
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,BUILDROOT,SRPMS}
 cp ../R-3.1.2.tar.gz rpmbuild/SOURCES
-cp ../packages/RevoBase.tar.gz rpmbuild
-cd rpmbuild/SOURCES
-tar xzf R-3.1.2.tar.gz
-mv R-3.1.2 RRO-8.0.1
-tar czf RRO-8.0.1.tar.gz RRO-8.0.1
-rm R-3.1.2.tar.gz
-cd ../
+#cp ../packages/RevoBase.tar.gz rpmbuild
+#cd rpmbuild/SOURCES
+#tar xzf R-3.1.2.tar.gz
+#mv R-3.1.2 RRO-8.0.1
+#tar czf RRO-8.0.1.tar.gz RRO-8.0.1
+#rm R-3.1.2.tar.gz
+#cd ../
 rpmbuild -ba SPECS/R.spec

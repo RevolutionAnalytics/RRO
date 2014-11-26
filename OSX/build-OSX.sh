c4487d93d789eecbe3c24dd06c70f8424a4ed4cc
root <root@cloud-server-22.(none)>
2014-11-26 13:10:28 -0500

    cleaning up scripts [ci skip]

--- OSX/build-OSX.sh
+++ OSX/build-OSX.sh
@@ -11,14 +11,6 @@ cp files/intro.txt OSX/project
 cd OSX
 tar xzf NcFTP.tar.gz
 sudo installer -package NcFTP.pkg -target /
-mkdir mkl
-cd mkl
-ncftp -u ftpuser -p revo-ftp 23.253.35.131 << FOO1
-cd lib
-mget *.dylib
-quit
-FOO1
-cd ../
 tar xzf ../R-3.1.1.tar.gz
 cp Makefile.fw R-3.1.1
 brew install cairo
@@ -30,9 +22,10 @@ sudo tar xf /Users/travis/build/RevolutionAnalytics/RRO/OSX/gfortran-4.8.2-darwi
 cd /Users/travis/build/RevolutionAnalytics/RRO/OSX
 mkdir rd64
 cd rd64
-export MKLROOT="/Users/travis/build/RevolutionAnalytics/RRO/OSX/mkl"
-export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -lmkl_gf_ilp64"
-../R-3.1.1/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' '--with-blas="${MKL}"' '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' 'CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include' '--x-libraries=/opt/X11/lib' '--with-libtiff=no'
+## export MKLROOT="/Users/travis/build/RevolutionAnalytics/RRO/OSX/mkl"
+## export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -lmkl_gf_ilp64"
+## ../R-3.1.1/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' '--with-blas="${MKL}"' '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' 'CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include' '--x-libraries=/opt/X11/lib' '--with-libtiff=no'
+../R-3.1.1/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' 'CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include' '--x-libraries=/opt/X11/lib' '--with-libtiff=no'
 make
 bin/R CMD INSTALL ../../packages/Revobase_OSX_7.3.0.tgz
 cp /usr/local/lib/libquadmath.0.dylib lib
@@ -65,8 +58,3 @@ sudo installer -package Packages.pkg -target /
 cd /Users/travis/build/RevolutionAnalytics/RRO/OSX/project
 /usr/local/bin/packagesbuild RevolutionBasic.pkgproj
 cp ./build/RevolutionBasic.pkg R-3.1.1.pkg 
-ncftp -u ftpuser -p revo-ftp 23.253.35.131 << FOO/project
-rm R-3.1.1.pkg
-put R-3.1.1.pkg
-quit
-FOO

8b067f615bef26073877a66ee1334c1697c9a2b6
root <root@cloud-server-22.(none)>
2014-11-27 13:21:44 -0500

    cleaning up scripts

--- OSX/build-OSX.sh
+++ OSX/build-OSX.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+cd ../
 uname -a
 pwd
 BUILD_DIR=/Users/travis/build/RevolutionAnalytics
 export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
-cd ../
 cp COPYING OSX/project
 cp COPYING COPYING.txt
 cp README-legal.txt OSX/project
@@ -21,7 +21,7 @@ cd rd64
 ### export MKLROOT="$BUILD_DIR/RRO/OSX/mkl"
 ### export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_intel -lmkl_core -lmkl_intel_ilp64 -lmkl_intel_thread -lpthread -lm"
 ### ../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' --with-blas="${MKL}" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=no' 
-../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=no' 
+../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' --with-blas="-framework Accelerate" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=yes' 
 mkdir lib
 ###cp ../mkl/libiomp5.dylib lib
 ###cp ../mkl/libmkl_avx.dylib lib
@@ -51,7 +51,6 @@ cd Mac-GUI-1.65
 xcodebuild -target "Revolution R Open" 
 cd ../
 sudo cp -a Mac-GUI-1.65/build/Release/Revo*.app /Applications
-ls -l /Applications
 ## make package
 cd $BUILD_DIR/RRO/OSX/project
 /usr/local/bin/packagesbuild RevolutionBasic.pkgproj

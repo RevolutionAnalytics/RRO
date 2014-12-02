e38451a9ab39abca8b8802f08f5b58330e5c8ca6
root <root@cloud-server-22.(none)>
2014-12-02 07:21:46 -0500

    update checkpoint date to 2014-12-01

--- Windows/checkpoint.R
+++ Windows/checkpoint.R
@@ -1,1 +1,1 @@
-install.packages("checkpoint",repos="http://mran.revolutionanalytics.com/snapshot/2014-11-20")
+install.packages("checkpoint",repos="http://mran.revolutionanalytics.com/snapshot/2014-12-01")

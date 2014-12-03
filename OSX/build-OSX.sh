3bd6d09f724107cdc004444680f415a2bfdab47b
root <root@cloud-server-22.(none)>
2014-12-03 16:53:01 -0500

    update and add files for RRO.framework

--- OSX/build-OSX.sh
+++ OSX/build-OSX.sh
@@ -3,7 +3,7 @@
 cd ../
 uname -a
 pwd
-BUILD_DIR=/Users/builder/
+BUILD_DIR=/Users/nriesland
 export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
 cp COPYING OSX/project
 cp COPYING COPYING.txt
@@ -12,26 +12,55 @@ cp README.txt OSX/project
 cp RRO-NEWS.txt OSX/project
 cp files/intro.txt OSX/project
 cd OSX
+### build RRO.framework
+rm -rf R-3.1.2
+rm -rf rd64-RRO
+tar xzf ../R-3.1.2.tar.gz
+cp Makefile-RRO.fw R-3.1.2/Makefile.fw
+cp Makeconf-RRO.in R-3.1.2/Makeconf.in
+mkdir rd64-RRO
+cd rd64-RRO
+export MKLROOT="/opt/intel/composer_xe_2015.0.077/mkl"
+# bad export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_intel_ilp64 -lmkl_intel_sequential -lmkl_core -lpthread -lm"
+## works export MKL=" -L${MKLROOT}/lib -lmkl_intel -lmkl_core -lmkl_intel_thread "
+export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_intel -lmkl_core -lmkl_intel_thread "
+../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' --with-blas="${MKL}" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=yes' --disable-openmp
+mkdir lib
+cp /opt/intel/composer_xe_2015.0.077/compiler/lib/libiomp5.dylib lib
+cp $MKLROOT/lib/libmkl_avx.dylib lib
+cp $MKLROOT/lib/libmkl_core.dylib lib
+cp $MKLROOT/lib/libmkl_intel_ilp64.dylib lib
+cp $MKLROOT/lib/libmkl_intel_lp64.dylib lib
+cp $MKLROOT/lib/libmkl_intel_thread.dylib lib
+cp $MKLROOT/lib/libmkl_mc.dylib lib
+make
+bin/R CMD INSTALL $BUILD_DIR/RRO/packages/Revobase_OSX_7.3.0.tgz
+cp /usr/local/lib/libquadmath.0.dylib lib
+cp /usr/local/lib/libgfortran.3.dylib lib
+cp /usr/local/lib/libgcc_s_x86_64.1.dylib lib
+cp /usr/local/lib/libgcc_s.1.dylib lib
+cp /usr/local/opt/readline/lib/libreadline.6.3.dylib lib
+sudo make install
+sudo ln -s /Library/Frameworks/RRO.framework/Libraries/libreadline.6.3.dylib /Library/Frameworks/RRO.framework/Libraries/libreadline.dylib
+sudo cp $BUILD_DIR/RRO/files/Rprofile.site /Library/Frameworks/RRO.framework/Resources/etc
+sudo cp $BUILD_DIR/RRO/COPYING /Library/Frameworks/RRO.framework
+sudo cp $BUILD_DIR/RRO/README-legal.txt /Library/Frameworks/RRO.framework
+sudo cp $BUILD_DIR/RRO/README.txt /Library/Frameworks/RRO.framework
+sudo cp $BUILD_DIR/RRO/RRO-NEWS.txt /Library/Frameworks/RRO.framework
+## done building RRO.framework
+
+cd $BUILD_DIR/RRO/OSX
+rm -rf R-3.1.2
+rm -rf rd64
 tar xzf ../R-3.1.2.tar.gz
 cp Makefile.fw R-3.1.2
 # brew install cairo
 # brew install jpeg
 mkdir rd64
 cd rd64
-### export MKLROOT="$BUILD_DIR/RRO/OSX/mkl"
-### export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_intel -lmkl_core -lmkl_intel_ilp64 -lmkl_intel_thread -lpthread -lm"
-### ../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' --with-blas="${MKL}" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=no' 
 ../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' --with-blas="-framework Accelerate" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=yes' 
 mkdir lib
-###cp ../mkl/libiomp5.dylib lib
-###cp ../mkl/libmkl_avx.dylib lib
-###cp ../mkl/libmkl_core.dylib lib
-###cp ../mkl/libmkl_intel_ilp64.dylib lib
-###cp ../mkl/libmkl_intel_lp64.dylib lib
-###cp ../mkl/libmkl_intel_thread.dylib lib
-###cp ../mkl/libmkl_mc.dylib lib
 make
-### bin/R CMD INSTALL ../../packages/Revobase_OSX_7.3.0.tgz
 cp /usr/local/lib/libquadmath.0.dylib lib
 cp /usr/local/lib/libgfortran.3.dylib lib
 cp /usr/local/lib/libgcc_s_x86_64.1.dylib lib

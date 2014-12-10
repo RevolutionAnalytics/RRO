cd072168ce860baf25e31f4c30e91b4f2765c606
root <root@cloud-server-22.(none)>
2014-12-10 09:46:00 -0500

    use gfortran4.6 for 12.04

--- Ubuntu/build.sh
+++ Ubuntu/build.sh
@@ -13,9 +13,5 @@ cd ../
 rpmbuild -ba SPECS/R.spec
 cd RPMS/x86_64
 alien --scripts --to-deb RRO-8.0.1-3.1.2-1.x86_64.rpm
-grepout=`grep -i "gfortran-4.8" /etc/lsb-release`
-      if [ 0 -ne ${#grepout} ] ; then
-             sed -i -e "s/gfortran-4.8/gfortran-4.6/" $HOME/install.sh
-      fi
 cp $HOME/install.sh .
 mv rro-8.0.1_3.1.2-2_amd64.deb RRO_3.1.2-1_amd64.deb

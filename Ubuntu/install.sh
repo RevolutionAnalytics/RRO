62d68f9d1dd25cc5b0a3160602bebca4dae27a37
Chris Mosetick <chris@revolutionanalytics.com>
2014-12-12 12:55:29 -0800

    update install.sh to work on both Ubuntu platforms

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -3,7 +3,7 @@ echo "Installing RRO-8.0.1-Beta"
 rm /usr/bin/R 2>/dev/null 1>/dev/null
 rm /usr/bin/Rscript 2>/dev/null 1>/dev/null
 apt-get -y install gcc g++ gfortran make liblzma-dev 2>/dev/null 1>/dev/null
-dpkg -i RRO-8.0.1-Beta-Ubuntu-14.04.x86_64.deb 2>/dev/null 1>/dev/null
-if [ "$?" -ne 0 ]; then 
+dpkg -i RRO-8.0.*-Beta-Ubuntu*.deb 2>/dev/null 1>/dev/null
+if [ "$?" -ne 0 ]; then
 apt-get -y -f install
 fi

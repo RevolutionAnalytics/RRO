0234632c172b1b71ff7129db3fefa39206771634
root <root@cloud-server-22.(none)>
2014-12-17 11:53:31 -0500

    update version to beta3
    Cherry-pick from master->dev file out of sync.
    Conflicts:
    	Ubuntu/install.sh

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+<<<<<<< HEAD
 echo "Installing RRO-8.0.2-Beta"
+=======
+echo "Installing RRO-8.0.1-Beta3"
+>>>>>>> a562259... update version to beta3
 rm /usr/bin/R 2>/dev/null 1>/dev/null
 rm /usr/bin/Rscript 2>/dev/null 1>/dev/null
 apt-get -y install gcc g++ gfortran make liblzma-dev 2>/dev/null 1>/dev/null
-dpkg -i RRO-8.0.*-Beta-Ubuntu*.deb 2>/dev/null 1>/dev/null
+dpkg -i RRO-8.0.*-Beta*-Ubuntu*.deb 2>/dev/null 1>/dev/null
 if [ "$?" -ne 0 ]; then
 apt-get -y -f install
 fi

dc9066325b5d2cadeb21f978c8767cb72ef96f1f
Chris Mosetick <chris@revolutionanalytics.com>
2015-01-20 12:34:02 -0800

    update Mac build script, Packages installer for README-legal.txt, change pkg filename

--- OSX/build-OSX.sh
+++ OSX/build-OSX.sh
@@ -6,30 +6,30 @@ sudo rm -rf /Library/Frameworks/R.framework
 sudo rm -rf /Library/Frameworks/RRO.framework
 sudo rm -rf /Applications/Revo*.app
 cd ../
+
 BUILD_RRO_FRAMEWORK=0
 pwd
 PWDD=`pwd`
 BUILD_DIR=$PWDD
 export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
 cp COPYING OSX/project
-cp COPYING COPYING.txt
-cp README-legal.txt OSX/project
-cp README.txt OSX/project
-cp RRO-NEWS.txt OSX/project
+cp OSX/README-legal.txt OSX/project
+cp OSX/README.txt OSX/project
 cp files/intro.txt OSX/project
+
 if [ $BUILD_RRO_FRAMEWORK -eq 1 ] ; then
 cd OSX
 ### build RRO.framework
-rm -rf R-3.1.2
+#rm -rf R-3.1.2
 rm -rf rd64-RRO
-tar xzf ../R-3.1.2.tar.gz
-cp Makefile-RRO.fw R-3.1.2/Makefile.fw
-cp Makeconf-RRO.in R-3.1.2/Makeconf.in
-mkdir rd64-RRO
-cd rd64-RRO
-export MKLROOT="/opt/intel/mkl"
-export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_rt "
-../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' --with-blas="${MKL}" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=yes' --disable-openmp
+#tar xzf ../R-3.1.2.tar.gz
+cp Makefile-RRO.fw R-src/Makefile.fw
+cp Makeconf-RRO.in R-src/Makeconf.in
+#mkdir rd64-RRO
+cd rd64
+#export MKLROOT="/opt/intel/mkl"
+#export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_rt "
+../../R-src/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang'  'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2'  --with-blas="${MKL}" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--x-includes=/opt/X11/include' '--with-libtiff=yes' --disable-openmp
 mkdir lib
 cp $MKLROOT/lib/libiomp5.dylib lib
 cp $MKLROOT/lib/libmkl_avx.dylib lib
@@ -56,17 +56,19 @@ sudo cp $BUILD_DIR/README.txt /Library/Frameworks/RRO.framework
 sudo cp $BUILD_DIR/RRO-NEWS.txt /Library/Frameworks/RRO.framework
 ## done building RRO.framework
 fi
+### End of MKL RRO Framework
+
 
 cd $BUILD_DIR/OSX
-rm -rf R-3.1.2
+#rm -rf R-3.1.2
 rm -rf rd64
-tar xzf ../R-3.1.2.tar.gz
-cp Makefile.fw R-3.1.2
+#tar xzf ../R-3.1.2.tar.gz
+cp Makefile.fw R-src
 # brew install cairo
 # brew install jpeg
 mkdir rd64
 cd rd64
-../R-3.1.2/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'F77=gfortran-4.8' 'FC=gfortran-4.8' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' 'FCFLAGS=-Wall -g -O2' 'F77FLAGS=-Wall -g -O2' --with-blas="-framework Accelerate" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--with-libtiff=yes' 
+../R-src/configure 'CC=clang' 'CXX=clang++' 'OBJC=clang' 'CFLAGS=-Wall -mtune=core2 -g -O2' 'CXXFLAGS=-Wall -mtune=core2 -g -O2' 'OBJCFLAGS=-Wall -mtune=core2 -g -O2' --with-blas="-framework Accelerate" '--with-lapack' '--with-system-zlib' '--enable-memory-profiling' "CPPFLAGS=-I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -DPLATFORM_PKGTYPE='\"mac.binary.mavericks\"'" '--x-libraries=/opt/X11/lib' '--x-includes=/opt/X11/include/' '--with-libtiff=yes'
 mkdir lib
 make
 cp /usr/local/lib/libquadmath.0.dylib lib
@@ -82,6 +84,7 @@ sudo cp $BUILD_DIR/README-legal.txt /Library/Frameworks/R.framework
 sudo cp $BUILD_DIR/README.txt /Library/Frameworks/R.framework
 sudo cp $BUILD_DIR/RRO-NEWS.txt /Library/Frameworks/R.framework
 cd $BUILD_DIR/OSX
+
 ## OS X GUI
 rm -rf Mac-GUI-1.65
 tar xzf Mac-GUI-1.65.tar.gz
@@ -91,4 +94,4 @@ cd ../
 sudo cp -a Mac-GUI-1.65/build/Release/Revo*.app /Applications
 ## make package
 cd $BUILD_DIR/OSX/project
-/usr/local/bin/packagesbuild RevolutionBasic.pkgproj
+/usr/local/bin/packagesbuild RRO.pkgproj

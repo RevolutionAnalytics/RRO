c3e1a3684873d9642995187a5f5d6af44f87f13b
Chris Mosetick <chris@revolutionanalytics.com>
2015-01-27 14:22:15 -0800

    Removing r-ver.tar.gz from spec file and build script.

--- CentOS/build.sh
+++ CentOS/build.sh
@@ -3,10 +3,9 @@ HOME=`pwd`
 export HOME
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,BUILDROOT,SRPMS}
-cp ../R-src rpmbuild/SOURCES
+cp -p ../R-src rpmbuild/SOURCES
 cp ../packages/RevoBase.tar.gz rpmbuild
 cd rpmbuild/SOURCES
-#tar xzf R-3.1.2.tar.gz
 mv R-src RRO-8.0.2-3.1.2
 tar czf RRO-8.0.2-3.1.2.tar.gz RRO-8.0.2-3.1.2
 rm -rf R-src RRO-8.0.2-3.1.2

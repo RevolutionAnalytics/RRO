d9dc7e17ec400ba5c845cde7d955124ef8706451
Chris Mosetick <chris@revolutionanalytics.com>
2015-01-27 15:00:15 -0800

    update Ubuntu spec file and build.sh

--- Ubuntu/build.sh
+++ Ubuntu/build.sh
@@ -4,12 +4,12 @@ cp ../packages/RevoBase.tar.gz rpmbuild
 HOME=`pwd`
 export HOME
 cd ../
-mv R-src RRO-8.0.1-3.1.2
-tar czf RRO-8.0.1-3.1.2.tar.gz RRO-8.0.1-3.1.2
-cp RRO-8.0.1-3.1.2.tar.gz Ubuntu/rpmbuild/SOURCES
+mv R-src RRO-8.0.2-3.1.2
+tar czf RRO-8.0.2-3.1.2.tar.gz RRO-8.0.2-3.1.2
+cp RRO-8.0.2-3.1.2.tar.gz Ubuntu/rpmbuild/SOURCES
 cd Ubuntu/rpmbuild
 rpmbuild -ba SPECS/R.spec
 cd RPMS/x86_64
-alien --scripts --to-deb RRO-8.0.1-3.1.2-1.x86_64.rpm
+alien --scripts --to-deb RRO-8.0.2-3.1.2-1.x86_64.rpm
 cp $HOME/install.sh .
-mv rro-8.0.1_3.1.2-2_amd64.deb RRO_3.1.2-1_amd64.deb
+mv rro-8.0.2_3.1.2-2_amd64.deb RRO_3.1.2-1_amd64.deb

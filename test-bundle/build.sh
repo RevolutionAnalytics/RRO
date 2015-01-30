2b28401092eb19acd517be757a21345ac2b2787e
root <root@cloud-server-22.(none)>
2015-01-30 11:17:11 -0500

    user R source tree instead of tar file

--- test-bundle/build.sh
+++ test-bundle/build.sh
@@ -7,8 +7,8 @@ cp -pr ../R-src rpmbuild/SOURCES
 
 cp ../packages/RevoBase.tar.gz rpmbuild
 cd rpmbuild/SOURCES
-mv R-src RRO-8.0.2-3.1.2
-tar czf RRO-8.0.2-3.1.2.tar.gz RRO-8.0.2-3.1.2
+mv R-src R-3.1.2
+tar czf R-3.1.2.tar.gz R-3.1.2
 rm -rf R-src RRO-8.0.2-3.1.2
 cd ../
 rpmbuild -ba SPECS/R.spec

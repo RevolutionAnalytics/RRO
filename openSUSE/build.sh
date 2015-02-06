4fe202db055bec3f4ced0869161a52d38a83c512
Nick Riesland <nriesland@localhost.localdomain>
2015-02-06 03:52:00 -0800

    Fix openSUSE to use R-src

--- openSUSE/build.sh
+++ openSUSE/build.sh
@@ -3,12 +3,10 @@ HOME=`pwd`
 export HOME
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,BUILDROOT,SRPMS}
-cp ../R-3.1.2.tar.gz rpmbuild/SOURCES
+cp -a ../R-src rpmbuild/SOURCES/RRO-8.0.1-3.1.2
 cp ../packages/RevoBase.tar.gz rpmbuild
 cd rpmbuild/SOURCES
-tar xzf R-3.1.2.tar.gz
-mv R-3.1.2 RRO-8.0.1-3.1.2
 tar czf RRO-8.0.1-3.1.2.tar.gz RRO-8.0.1-3.1.2
-rm -rf R-3.1.2.tar.gz RRO-8.0.1-3.1.2
+rm -rf RRO-8.0.1-3.1.2
 cd ../
 rpmbuild -ba SPECS/R.spec

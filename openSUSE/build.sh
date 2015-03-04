bae3bb5dea3d31a502f0dd03cfbdfc5f58b36698
root <root@cloud-server-22.(none)>
2015-03-04 14:45:17 -0500

    update version to 8.0.2

--- openSUSE/build.sh
+++ openSUSE/build.sh
@@ -3,10 +3,10 @@ HOME=`pwd`
 export HOME
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,BUILDROOT,SRPMS}
-cp -a ../R-src rpmbuild/SOURCES/RRO-8.0.1-3.1.2
+cp -a ../R-src rpmbuild/SOURCES/RRO-8.0.2-3.1.2
 cp ../packages/RevoBase.tar.gz rpmbuild
 cd rpmbuild/SOURCES
-tar czf RRO-8.0.1-3.1.2.tar.gz RRO-8.0.1-3.1.2
-rm -rf RRO-8.0.1-3.1.2
+tar czf RRO-8.0.2-3.1.2.tar.gz RRO-8.0.2-3.1.2
+rm -rf RRO-8.0.2-3.1.2
 cd ../
 rpmbuild -ba SPECS/R.spec

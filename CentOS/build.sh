c8d0e9fe5762b3016aa33ca2fcda016798b3dbb4
nriesland <nriesland@gmail.com>
2015-05-11 12:29:59 +0000

    update Revision to 3.2.0

--- CentOS/build.sh
+++ CentOS/build.sh
@@ -4,11 +4,11 @@ export HOME
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,BUILDROOT,SRPMS}
 cp -pr ../R-src rpmbuild/SOURCES
-cp ../packages/RevoBase.tar.gz rpmbuild
+# cp ../packages/RevoBase.tar.gz rpmbuild
 cd rpmbuild/SOURCES
-mv R-src RRO-8.0.3-3.1.3
-tar czf RRO-8.0.3-3.1.3.tar.gz RRO-8.0.3-3.1.3
-rm -rf R-src RRO-8.0.3-3.1.3
+mv R-src RRO-3.2.0-3.2.0
+tar czf RRO-3.2.0-3.2.0.tar.gz RRO-3.2.0-3.2.0
+rm -rf R-src RRO-3.2.0-3.2.0
 cd ../
 rpmbuild -ba SPECS/R.spec
 

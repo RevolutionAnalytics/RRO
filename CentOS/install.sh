a29fc0bee01e624b39ce9fa8ffa756cafb6d2a0b
nriesland <nriesland@gmail.com>
2015-05-11 13:33:45 +0000

    update Revision to 3.2.0

--- CentOS/install.sh
+++ CentOS/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Installing RRO-8.0.3"
+echo "Installing RRO-3.2.0"
 yum -y install make gcc gcc-gfortran 2>/dev/null 1>/dev/null
-yum -y --nogpgcheck localinstall RRO-8.*.x86_64.rpm 2>/dev/null 1>/dev/null
+yum -y --nogpgcheck localinstall RRO-2.*.x86_64.rpm 2>/dev/null 1>/dev/null
 

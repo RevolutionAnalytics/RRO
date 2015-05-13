2eead1835097d33ac62c714d7a48f51f3b8b114c
nriesland <nriesland@gmail.com>
2015-05-13 17:01:56 +0000

    update installers to 3.2.0

--- Ubuntu/install.sh
+++ Ubuntu/install.sh
@@ -3,7 +3,7 @@ echo "Installing RRO-3.2.0"
 rm /usr/bin/R 2>/dev/null 1>/dev/null
 rm /usr/bin/Rscript 2>/dev/null 1>/dev/null
 apt-get -y install gcc g++ gfortran make liblzma-dev 2>/dev/null 1>/dev/null
-dpkg -i RRO-8.0.*-Ubuntu*.deb 2>/dev/null 1>/dev/null
+dpkg -i RRO-3.2.*-Ubuntu*.deb 2>/dev/null 1>/dev/null
 if [ "$?" -ne 0 ]; then
 apt-get -y -f install
 fi

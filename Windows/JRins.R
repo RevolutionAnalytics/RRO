4c2baf1dbb2efdfbe21d6e4d3c60ef300cf1b9be
Nathan Sosnovske <nathan@sosnovske.com>
2015-06-23 10:50:16 -0700

    Update devel to 3.2.1 in installer build

--- Windows/JRins.R
+++ Windows/JRins.R
@@ -92,21 +92,21 @@
     writeLines(lines, con)
 
     writeLines(c("", "", "[Files]"), con)
-    cat('Source: "R-devel\\etc\\checkpoint.R', '"; ',
+    cat('Source: "R-3.2.1\\etc\\checkpoint.R', '"; ',
             'DestDir: "{app}\\etc', '"; ',
             'Flags: ignoreversion; ',
             'Components: main', 
             file = con, sep = "")
     cat("\n", file = con)
 
-    cat('Source: "R-devel\\etc\\README.txt', '"; ',
+    cat('Source: "R-3.2.1\\etc\\README.txt', '"; ',
             'DestDir: "{app}', '"; ',
             'Flags: ignoreversion; ',
             'Components: main', 
             file = con, sep = "")
     cat("\n", file = con)
 
-    cat('Source: "R-devel\\etc\\COPYING', '"; ',
+    cat('Source: "R-3.2.1\\etc\\COPYING', '"; ',
             'DestDir: "{app}', '"; ',
             'Flags: ignoreversion; ',
             'Components: main', 

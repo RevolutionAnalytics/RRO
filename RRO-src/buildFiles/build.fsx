e3ea54a19f7c68a61485941c025bec263fc8db34
Nathan Sosnovske <nathan@sosnovske.com>
2015-08-05 20:15:16 -0700

    Testing changes to script file

--- RRO-src/buildFiles/build.fsx
+++ RRO-src/buildFiles/build.fsx
@@ -4,6 +4,14 @@
 open Fake
 open RevoUtils
 
+let (+/) path1 path2 = System.IO.Path.Combine(path1, path2)
+
+let SCRIPT_DIR = __SOURCE_DIRECTORY__
+let RRO_DIR = System.IO.Directory.GetParent(SCRIPT_DIR).ToString()
+let BASE_DIR = System.IO.Directory.GetParent(RRO_DIR).ToString()
+let WORKSPACE = BASE_DIR +/ "workspace"
+
+
 let platform = RevoUtils.Platform.GetPlatform()
 let version = RevoUtils.Platform.GetReleaseVersion()
 
@@ -11,6 +19,9 @@ Target "Build" (fun _ ->
     trace "The build starts here."
     trace("The platform is " + platform.ToString())
     trace("The platform version is " + version.ToString())
+    trace("This script is executing in " + SCRIPT_DIR)
+
+    FileUtils.mkdir(WORKSPACE)
 )
 
 Run "Build"

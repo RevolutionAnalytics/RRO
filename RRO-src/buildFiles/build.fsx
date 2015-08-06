a23660edd638d25728039cf59c242e23fcb38788
Nathan Sosnovske <nathan@sosnovske.com>
2015-08-05 23:33:03 -0700

    use homedir for copy target

--- RRO-src/buildFiles/build.fsx
+++ RRO-src/buildFiles/build.fsx
@@ -51,7 +51,7 @@ Target "Build_Linux" (fun _ ->
 
     ignore(Shell.Exec("tar", "czf RRO-" + RRO_VERSION + ".tar.gz RRO-" + RRO_VERSION, WORKSPACE))
     FileUtils.cp (WORKSPACE +/ "RRO-" + RRO_VERSION + ".tar.gz") ("~/rpmbuild/SOURCES/")
-    FileUtils.cp (RRO_DIR +/ "files/linux/spec" +/ "R_" + flavor.ToString() + ".spec") ("~/rpmbuild/SOURCES")
+    FileUtils.cp (RRO_DIR +/ "files/linux/spec" +/ "R_" + flavor.ToString() + ".spec") (homeDir +/ "/rpmbuild/SOURCES")
     ()
 )
 

be4970a89e21868cb99134a6951ff27da599991d
Nathan Sosnovske <nsosnov@microsoft.com>
2015-08-12 14:51:24 -0700

    Add windows binary packages

--- RRO-src/buildFiles/build.fsx
+++ RRO-src/buildFiles/build.fsx
@@ -145,17 +145,15 @@ Target "Build_Linux" (fun _ ->
 Target "Build_Windows" (fun _ ->
     trace "Entered Windows Logic"
 
+    //Verify that our build environment is sane. This section still needs work and tools to be added.
     let tools = VerifyWindowsTools
-
     trace ("Rtools found at " + tools.["Rtools"])
     trace ( "MiKTeX found at " + tools.["MiKTeX"])
     trace ( "Inno Setup found at " + tools.["Inno Setup"])
     trace ( "Strawberry Perl found at " + tools.["Perl"])
-
     let path = environVar "PATH"
     setProcessEnvironVar "PATH" (tools.["Rtools"] +/ "bin;" + tools.["Rtools"] +/ "gcc-4.6.3\\bin;" + tools.["MiKTeX"] +/ "miktex\\bin;" + tools.["Perl"] +/ "perl\\bin;" + tools.["Inno Setup"] + ";" + path)
     trace ("PATH IS " + (environVar "PATH"))
-
     FileUtils.mkdir(WORKSPACE)
     FileUtils.mkdir(PKG_DIR)
 
@@ -172,12 +170,9 @@ Target "Build_Windows" (fun _ ->
         let url = package.Value("location")
         webClient.DownloadFile(url.ToString(), (PKG_DIR +/ package.Value("destFileName")))
         System.IO.File.WriteAllText((PKG_DIR +/ package.Value("name") + ".tgz"), package.Value("destFileName"))
-
-        //Add it to package list string
-        
         extraPackageList <- extraPackageList + " " + package.Value("name")
           
-    //Now build it
+    //Prep directories, copying over custom files
     let rDir = WORKSPACE +/ "R-" + R_VERSION
     let gnuWin32Dir = rDir +/ "src" +/ "gnuwin32"
     let installerDir = gnuWin32Dir +/ "installer"
@@ -186,9 +181,7 @@ Target "Build_Windows" (fun _ ->
     let etcFiles = [ WINDOWS_FILES_DIR +/ "checkpoint.R"; WINDOWS_FILES_DIR +/ "REV_14419_Clark_2C.ico"; BASE_DIR +/ "README.txt"; BASE_DIR +/ "COPYING" ]
     let installerFiles = [ WINDOWS_FILES_DIR +/ "clarkSmall.bmp"; WINDOWS_FILES_DIR +/ "Makefile"; WINDOWS_FILES_DIR +/ "header1.iss";
                            WINDOWS_FILES_DIR +/ "reg3264.iss"; WINDOWS_FILES_DIR +/ "JRins.R"; COMMON_FILES_DIR +/ "intro.txt"; ]
-
-    
-
+   
     FileUtils.mkdir(WORKSPACE +/ "tmp")
     FileUtils.cp_r (BASE_DIR +/ "R-src") (WORKSPACE +/ "R-" + R_VERSION)
     FileUtils.cp_r ("c:\\R64\\Tcl") (WORKSPACE +/ "R-" + R_VERSION +/ "Tcl")
@@ -199,7 +192,6 @@ Target "Build_Windows" (fun _ ->
     ReplaceInFiles [ (":::BUILDID:::", "\"1\"") ] [ (gnuWin32Dir +/ "fixed" +/ "etc" +/ "Rprofile.site") ]
     ReplaceInFiles [ (":::EXTRA_PACKAGES:::", extraPackageList) ] [ rDir +/ "share" +/ "make" +/ "vars.mk" ] 
 
-    
     for file in etcFiles do
         FileUtils.cp file (rDir +/ "etc")
     for file in installerFiles do
@@ -207,22 +199,31 @@ Target "Build_Windows" (fun _ ->
 
     
 
-    //invoke
+    //invoke build
     setProcessEnvironVar "tmpdir" (WORKSPACE +/ "tmp")
-    ignore(Shell.Exec("make", "-j8 distribution", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 all", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 cairodevices", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 recommended", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 vignettes", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 manuals", gnuWin32Dir))
+
+    //Stage binary packages
+    let binaryPackages = jsonObject.GetValue("binary_packages")
+    let mutable extraBinaryPackageList = ""
+    for package in binaryPackages do
+        //Download the package
+        use webClient = new System.Net.WebClient()
+        let url = package.Value("location")
+        webClient.DownloadFile(url.ToString(), (PKG_DIR +/ package.Value("destFileName")))
+        let source = Fake.FileSystemHelper.fileInfo (PKG_DIR +/ package.Value("destFileName"))
+        let target = Fake.FileSystemHelper.directoryInfo (rDir +/ "library")
+        Fake.ArchiveHelper.Zip.Extract target source
+        FileUtils.rm_rf ( rDir +/ "library" +/ package.Value("name") +/ "libs" +/ "i386" )
+        extraBinaryPackageList <- extraBinaryPackageList + " " + package.Value("name")
+
+    //Create the installer
+    ignore(Shell.Exec("make", "rinstaller EXTRA_PKGS=\'" + extraBinaryPackageList + "\'", gnuWin32Dir))
     ()
-)
-
-Target "TestParse" (fun _ ->
-    let fileContents = System.IO.File.ReadAllText("packages-default.json")
-    let jsonParser = Newtonsoft.Json.Linq.JObject.Parse(fileContents)
-
-    let packages = jsonParser.GetValue("packages")
-    for package in packages do
-        trace(package.Value("name").ToString())
-
-    ()
-
 )
 
 Target "Default" (fun _ ->

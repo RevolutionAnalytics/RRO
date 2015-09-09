b468c5b9cacd0aa1dabcd6f103440d1cfbf90761
Nathan Sosnovske <nsosnov@microsoft.com>
2015-09-09 16:41:33 -0700

    overwrite archive instead of deleting

--- RRO-src/buildFiles/build.fsx
+++ RRO-src/buildFiles/build.fsx
@@ -279,8 +279,6 @@ Target "Build_Windows" (fun _ ->
     
     if fileExists (PKG_DIR +/ "RevoUtils_" + RRC_VERSION + ".tar.gz") then
         ArchiveHelper.Tar.GZip.Extract (System.IO.DirectoryInfo(tmpDir)) (System.IO.FileInfo((PKG_DIR +/ "RevoUtils_" + RRC_VERSION + ".tar.gz")))
-        FileUtils.rm (PKG_DIR +/ "RevoUtils_" + RRC_VERSION + ".tar.gz")
-
         RegexReplaceInFileWithEncoding ":::RevoBuildID:::" BUILD_ID (System.Text.ASCIIEncoding()) (tmpDir +/ "RevoUtils" +/ "DESCRIPTION")
         ignore(ArchiveHelper.Tar.GZip.CompressDirWithDefaults (System.IO.DirectoryInfo(tmpDir +/ "RevoUtils")) (System.IO.FileInfo((PKG_DIR +/ "RevoUtils_" + RRC_VERSION + ".tar.gz"))))
         

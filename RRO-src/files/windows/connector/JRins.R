bc78d8ef994fb92579685478a752419a7e9c3763
Nathan Sosnovske <nsosnov@microsoft.com>
2015-09-29 12:10:01 -0700

    Change more underscores to dashes in connector installer

--- RRO-src/files/windows/connector/JRins.R
+++ RRO-src/files/windows/connector/JRins.R
@@ -57,7 +57,7 @@
         paste("AppVerName=RRO for RRE ", Rver, sep = ""),
         paste("AppVersion=", Rver, sep = ""),
         paste("VersionInfoVersion=", Rver0, sep = ""),
-        paste("DefaultDirName={code:UserPF}\\RRO\\RRO-", RROver, "_for_RRE-", Rver, "\\", "R-", RROver, sep = ""),
+        paste("DefaultDirName={code:UserPF}\\RRO\\RRO-", RROver, "-for-RRE-", Rver, "\\", "R-", RROver, sep = ""),
         ## paste("InfoBeforeFile=", srcdir, "\\doc\\COPYING", sep = ""),
         paste("InfoBeforeFile=intro.txt", sep=""),
         if(Producer == "R-core") "AppPublisher=Revolution Analytics"

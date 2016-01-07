1afeccb0b1b5249863bcde0aa162252534d17c1e
Nathan Sosnovske <nathan@sosnovske.com>
2016-01-07 10:09:43 -0800

    Remove tty requirement in docker build

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -f ${DIR}/r-linux.tar.gz
 
-docker run -v ${DIR}:/io -i -t nathansoz/centos:4 /io/build2.sh
+docker run -v ${DIR}:/io -i nathansoz/centos:4 /io/build2.sh
 
 tar zcvf ${DIR}/r-linux.tar.gz ${DIR}/build-output

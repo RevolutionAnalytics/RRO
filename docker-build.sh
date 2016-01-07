3d431396b833823c4c5f30bfeb11cafd18c6adbd
Nathan Sosnovske <nathan@localhost.localdomain>
2016-01-06 16:02:34 -0800

    Tar up artifacts

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+rm -f ${DIR}/r-linux.tar.gz
+
 docker run -v ${DIR}:/io -i -t nathansoz/centos:4 /io/build2.sh
+
+tar zcvf ${DIR}/r-linux.tar.gz ${DIR}/build-output

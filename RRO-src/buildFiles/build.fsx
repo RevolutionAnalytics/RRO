c33603a675e77725a5ae3f26db88c6efa932ff34
Keith Schaab <keithsc@microsoft.com>
2016-02-01 21:32:33 -0800

    Adding multiple job builds

--- RRO-src/buildFiles/build.fsx
+++ RRO-src/buildFiles/build.fsx
@@ -339,11 +339,11 @@ Target "Build_Windows" (fun _ ->
 
     //invoke build
     setProcessEnvironVar "tmpdir" (WORKSPACE +/ "tmp")
-    ignore(Shell.Exec("make", "-j all", gnuWin32Dir))
-    ignore(Shell.Exec("make", "-j cairodevices", gnuWin32Dir))
-    ignore(Shell.Exec("make", "-j recommended", gnuWin32Dir))
-    ignore(Shell.Exec("make", "-j vignettes", gnuWin32Dir))
-    ignore(Shell.Exec("make", "-j manuals", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 all", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 cairodevices", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 recommended", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 vignettes", gnuWin32Dir))
+    ignore(Shell.Exec("make", "-j8 manuals", gnuWin32Dir))
 
     //Stage binary packages
     let binaryPackages = jsonObject.GetValue("binary_packages")

d675646bf03816cde80a10d0f731a2f70b03de07
Nathan Sosnovske <nathan@sosnovske.com>
2016-02-08 15:24:00 -0800

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -2,6 +2,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo $NUGET_PASSWORD
 rm -f ${DIR}/r-linux.tar.gz
 
-docker run -v ${DIR}:/io -i nathansoz/centos:4 /io/build2.sh
+docker run -v ${DIR}:/io -i mrsdocker.cloudapp.net:5000/mro /io/build2.sh
 
 tar zcvf ${DIR}/r-linux.tar.gz -C ${DIR} build-output

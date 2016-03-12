d08c7c3e889eacbbbf538ff825fee3ba41eb6698
Derek K Brown <derbrown@microsoft.com>
2016-03-11 22:41:25 -0800

    first pass at windows test

--- test/utils/runAllTests.sh
+++ test/utils/runAllTests.sh
@@ -1,12 +1,12 @@
 
-#keep moving if tests fail
-set +e
-
 set +x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 chmod +x ${DIR}/../entryPoints/*.sh
-${DIR}/../entryPoints/setup.sh != 0 || exit 1
+source ${DIR}/../entryPoints/setup.sh
+
+#keep moving if tests fail
+set +e
 
 let RETVAL=0
 for FILE in ${DIR}/../entryPoints/[1234567890]*.sh; do
@@ -14,6 +14,8 @@ for FILE in ${DIR}/../entryPoints/[1234567890]*.sh; do
     ${FILE}
     let RETVAL=${RETVAL}+$?
 done
+
+set -e
 
 ${DIR}/../entryPoints/cleanup.sh
 

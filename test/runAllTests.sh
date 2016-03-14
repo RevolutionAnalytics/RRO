f2645467be35081a13c1c370ea033a6aaaeaec93
Derek K Brown <derbrown@microsoft.com>
2016-03-14 10:37:56 -0700

    move runAllTests to more standard place

--- test/runAllTests.sh
+++ test/runAllTests.sh
@@ -1,15 +1,16 @@
 
 set +x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+REPOROOT="$(dirname ${DIR})"
 
-chmod +x ${DIR}/../entryPoints/*.sh
-source ${DIR}/../entryPoints/setup.sh
+chmod +x ${REPOROOT}/test/entryPoints/*.sh
+source ${REPOROOT}/test/entryPoints/setup.sh
 
 #keep moving if tests fail
 set +e
 
 let RETVAL=0
-for FILE in ${DIR}/../entryPoints/[1234567890]*.sh; do
+for FILE in ${REPOROOT}/entryPoints/[1234567890]*.sh; do
     echo Running ${FILE} ...
     ${FILE}
     let RETVAL=${RETVAL}+$?
@@ -17,7 +18,7 @@ done
 
 set -e
 
-${DIR}/../entryPoints/cleanup.sh
+${REPOROOT}/entryPoints/cleanup.sh
 
 exit ${RETVAL}
 
